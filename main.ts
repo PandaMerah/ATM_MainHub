30ea3e07ad2c41fb5569e445d6948cab27c1218b
PandaMerah <38294614+PandaMerah@users.noreply.github.com>
2022-03-24 18:37:46 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@ function intruder () {
         alarm()
         NotiMainBreached()
         serial.writeLine("INTRUDER DETECTED")
-        while (pins.digitalReadPin(DigitalPin.P3) == 1) {
+        while (IsThereIntruder >= 1) {
             strip.showColor(neopixel.colors(NeoPixelColors.Red))
             strip.show()
             basic.pause(100)
@@ -30,6 +30,9 @@ function alarm () {
 function WaitForKeypin () {
 	
 }
+function Light () {
+	
+}
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "NodeBreached") {
         serial.writeLine("Node Detect Intruder")
@@ -48,23 +51,24 @@ radio.onReceivedString(function (receivedString) {
 function worker () {
     strip.showColor(neopixel.colors(NeoPixelColors.White))
 }
-let DoorTimer = 0
 let SecurityPin = 0
 let AlarmStatus = 0
 let WorkingTimeOrNot = 0
 let IsThereIntruder = 0
 let strip: neopixel.Strip = null
+serial.writeLine("____________SYSTEM INITIALIZING____________")
+let ds = DS1302.create(DigitalPin.P13, DigitalPin.P14, DigitalPin.P15)
+ds.start()
 led.enable(false)
 strip = neopixel.create(DigitalPin.P16, 8, NeoPixelMode.RGB)
 esp8266.init(SerialPin.P2, SerialPin.P1, BaudRate.BaudRate115200)
 esp8266.connectWiFi("PandaRouter", "Panda1234")
+esp8266.initInternetTime(8)
+esp8266.updateInternetTime()
 serial.redirectToUSB()
-if (esp8266.isWifiConnected()) {
-    esp8266.initInternetTime(8)
-    esp8266.updateInternetTime()
-    serial.writeLine("CT -" + "" + ":" + "")
-    serial.writeLine("CT -" + esp8266.getHour() + ":" + esp8266.getMinute())
-}
+serial.writeLine("Current Time : " + ds.getHour() + ":" + ds.getMinute())
+serial.writeLine("Current Date : " + ds.getDay() + "/" + ds.getMonth() + "/" + ds.getYear())
+serial.writeLine("____________ATM MACHINE HAS INITIALIZED____________")
 loops.everyInterval(500, function () {
     if (pins.digitalReadPin(DigitalPin.P3) == 1) {
         serial.writeLine("Pintu Buka")
@@ -73,8 +77,8 @@ loops.everyInterval(500, function () {
             serial.writeLine("Pekerja")
         }
         if (WorkingTimeOrNot == 1 && AlarmStatus == 1) {
+            strip.showColor(neopixel.colors(NeoPixelColors.Purple))
             if (pins.digitalReadPin(DigitalPin.P4) == 0) {
-                strip.showColor(neopixel.colors(NeoPixelColors.Purple))
                 basic.pause(500)
                 if (SecurityPin == 0) {
                     intruder()
@@ -108,31 +112,30 @@ basic.forever(function () {
     if (input.buttonIsPressed(Button.B) && !(input.buttonIsPressed(Button.A))) {
         music.playTone(262, music.beat(BeatFraction.Whole))
         basic.pause(200)
-        serial.writeLine("Button Pressed")
+        SecurityPin = 0
         if (input.buttonIsPressed(Button.B) && !(input.buttonIsPressed(Button.A))) {
             music.playTone(262, music.beat(BeatFraction.Whole))
             basic.pause(200)
             SecurityPin = 0
-            serial.writeLine("Button Pressed")
             if (input.buttonIsPressed(Button.A) && !(input.buttonIsPressed(Button.B))) {
                 music.playTone(262, music.beat(BeatFraction.Whole))
                 basic.pause(200)
                 SecurityPin = 0
-                serial.writeLine("Button Pressed")
                 if (input.buttonIsPressed(Button.A) && !(input.buttonIsPressed(Button.B))) {
                     music.playTone(262, music.beat(BeatFraction.Whole))
                     basic.pause(200)
-                    serial.writeLine("Button Pressed")
-                    SecurityPin = 1
+                    serial.writeLine("ALARM HAS BEEN DISARMED")
                     music.stopAllSounds()
+                    IsThereIntruder = 0
+                    AlarmStatus = 0
+                    SecurityPin = 1
                 }
             }
         }
     }
     if (input.buttonIsPressed(Button.AB)) {
-        DoorTimer = 0
         music.playTone(262, music.beat(BeatFraction.Whole))
-        if (pins.digitalReadPin(DigitalPin.P3) == 0 || DoorTimer == 0) {
+        if (pins.digitalReadPin(DigitalPin.P3) == 0) {
             serial.writeLine("Alarm System Activated")
             music.playTone(988, music.beat(BeatFraction.Breve))
             AlarmStatus = 1
@@ -141,10 +144,10 @@ basic.forever(function () {
     }
 })
 control.inBackground(function () {
-    if (esp8266.getHour() < 19) {
+    if (ds.getHour() < 19) {
         WorkingTimeOrNot = 1
     }
-    if (esp8266.getHour() > 19) {
+    if (ds.getHour() > 19) {
         WorkingTimeOrNot = 0
     }
     basic.pause(5000)
